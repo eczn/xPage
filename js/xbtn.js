2b41eb08b2b919a746564a8a219a572b0e11a77c
eczn <1975845422@qq.com>
2016-08-15 02:29:46 +0800

    memu sys

---
+++ js/xbtn.js
@@ -0,0 +1,26 @@
+// xbtn.js 
+// xbtn的实现
+
+function memuInit(btnId,memu){
+	var status = 0; 
+	var $memu = $("#"+memu) || $(memu); 
+
+	$memu.css("opacity","0").css("display","none"); 
+	function click(){
+		if (status == 0){
+			status = 1;
+			$memu.css('display','block').animate({ opacity: 1 },800);
+
+		} else if (status == 1){
+			status = 0; 
+			// $memu.css('display','none');
+			$memu.animate({
+				opacity: 0
+			},800,function(){
+				$memu.css('display','none');
+			});
+		}
+	}
+
+	this.click = click;
+}
