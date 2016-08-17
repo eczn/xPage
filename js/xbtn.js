40cf8aff2d0831cebd58917d2c3e3421613fbb60
eczn <1975845422@qq.com>
2016-08-18 03:42:24 +0800

    一次比较大的更新了 比如 memu的新设计 还有 返回键 的引入

--- js/xbtn.js
+++ js/xbtn.js
@@ -5,20 +5,21 @@ function memuInit(btnId,memu){
 	var status = 0; 
 	var $memu = $("#"+memu) || $(memu); 
 
-	$memu.css("opacity","0").css("display","none"); 
+	// $memu.css("opacity","0").css("display","none");
+	 
 	function click(){
 		if (status == 0){
-			status = 1;
-			$memu.css('display','block').animate({ opacity: 1 },800);
+			// status = 1;
+			// $memu.css('display','block').animate({ opacity: 1 },800);
 			// $(".memu-item").addClass("memu-item-active");
 		} else if (status == 1){
-			status = 0; 
-			// $memu.css('display','none');
-			$memu.animate({
-				opacity: 0
-			},800,function(){
-				$memu.css('display','none');
-			});
+			// status = 0; 
+			// // $memu.css('display','none');
+			// $memu.animate({
+			// 	opacity: 0
+			// },800,function(){
+			// 	$memu.css('display','none');
+			// });
 		}
 	}
 
