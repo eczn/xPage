49f79e46c4523fd0561fa12cc1b2c430cbd28508
JimmyZhan <173760989@qq.com>
2016-08-20 12:30:43 +0800

    提交表格

---
+++ js/form.js
@@ -0,0 +1,36 @@
+//verify the form
+function verify(name,phone,intro) {
+	if (phone.length != 11 || !(/^1[3|4|5|7|8]\d{9}$/.test(phone))){
+			alert("请输入有效的手机号码！");
+			return; 
+		} 
+		else if (intro.length > 255) {
+			alert("个人介绍似乎有点长了...");
+			return; 
+		}
+        else if (name.length > 10) {
+			alert("名字填写错误");
+			return; 
+		}
+		$.ajax({
+			type: 'post',
+			async: true,
+			url: "http://eczn.website:8086", 
+			data: {
+				name: name,
+				phone: phone,
+				intro: intro
+			},
+			dataType: 'json',
+			success: function(data){
+				console.log(data);
+				console.log("success");
+				alert("报名成功！"); 
+			},
+			error: function(data){
+				console.warn("faild");
+				console.log("报名失败！ ");
+				console.log(data); 
+			}
+		});
+}
