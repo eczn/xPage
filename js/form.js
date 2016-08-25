692fa281a197f9fb500864205f2dc3c969636f36
eczn <1975845422@qq.com>
2016-08-26 01:28:58 +0800

    添加successInit构造函数

--- js/form.js
+++ js/form.js
@@ -1,12 +1,48 @@
-//verify the form
+
+// xPageForm a obj to achieve "WELCOME" when click JOIN US and SUCCEED. 
+
+function successInit(){
+	// 点下join us 成功的时候 onshow置于true，否则置于false，用来标识是否打开了welcome界面
+	var onshow = false; 
+	var $success = $("#success"); 
+
+	$success.css("display","none"); 
+
+	// when clicking join us
+	function click(){
+		if (onshow){
+			// from true to false;
+			onshow = false;
+
+			$success.css("display","none"); 
+		} else {
+			// from false to true;
+			onshow = true; 
+			$success.css("display","block"); 
+		}
+	}
+
+	this.click = click;
+}
+
+
+
 $(document).ready(function(){
+
+
 	$(".sent").click(function(){
+		
+
 		var name= $(".Name").val();
 		var phone= $(".Phone").val();
 		var intro= $(".Intro").val();
 		verify(name,phone,intro);
-	})
-})
+	});
+
+
+});
+
+//verify the form
 function verify(name,phone,intro) {
 	if (phone.length != 11 || !(/^1[3|4|5|7|8]\d{9}$/.test(phone))){
 			alert("请输入有效的手机号码！");
@@ -34,7 +70,8 @@ function verify(name,phone,intro) {
 				console.log(data);
 				console.log("success");
 				alert("报名成功！"); 
-				 $(".frame").val('');
+				$(".frame").val('');
+				xPageForm.click(); 
 			},
 			error: function(data){
 				console.warn("faild");
@@ -44,3 +81,4 @@ function verify(name,phone,intro) {
 			}
 		});
 }
+
