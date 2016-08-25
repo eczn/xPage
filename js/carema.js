8bfa2d969ae1af62d3e96adbfa72fab1a82dd878
CccZss <1045635547@qq.com>
2016-08-25 12:43:17 +0800

    修改宽度为百分比

--- js/carema.js
+++ js/carema.js
@@ -7,22 +7,22 @@ $(function(){
 		if(shadow.attr("class")=="shadow"){
 			$(this).removeClass("focus");
 			shadow.addClass("shadow_to_go");
-			$("#animate_lens").animate({"background-size": "300px"},200)
-			.animate({"background-size": "314px"}, 200, function(){
+			$("#animate_lens").animate({"background-size": "90%"},200)
+			.animate({"background-size": "98%"}, 200, function(){
 				setTimeout(function(){
 					blur.addClass("blur_to_go");
-				},1000); 
+				},800); 
 				setTimeout(function(){
 					content.addClass("content_to_go");
-				},2500); 
+				},1000); 
 				setTimeout(function(){
 					$("#animate_focus").addClass("focus");
-				},5200); 
+				},3200); 
 				
 			});
 		}else{
-			$("#animate_lens").animate({"background-size": "300px"},200)
-			.animate({"background-size": "314px"}, 200,function(){
+			$("#animate_lens").animate({"background-size": "90%"},200)
+			.animate({"background-size": "98%"}, 200,function(){
 				shadow.removeClass("shadow_to_go");
 				blur.removeClass("blur_to_go");
 				content.removeClass("content_to_go");
