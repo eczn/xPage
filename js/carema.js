560be703bffa67b950b8d3aee31c47a856510b5a
CccZss <1045635547@qq.com>
2016-08-25 13:58:56 +0800

    添加了闪光效果

--- js/carema.js
+++ js/carema.js
@@ -2,11 +2,15 @@ $(function(){
 	var content = $("#animate_content");
 	var shadow = $("#animate_shadow");
 	var blur = $("#animate_blur");
+	var flicker = $("#animate_flicker");
 
 	$("#animate_focus").click(function(){
 		if(shadow.attr("class")=="shadow"){
 			$(this).removeClass("focus");
 			shadow.addClass("shadow_to_go");
+			setTimeout(function(){
+				flicker.addClass("flicker");
+			},200); 
 			$("#animate_lens").animate({"background-size": "90%"},200)
 			.animate({"background-size": "98%"}, 200, function(){
 				setTimeout(function(){
@@ -14,10 +18,10 @@ $(function(){
 				},800); 
 				setTimeout(function(){
 					content.addClass("content_to_go");
-				},1000); 
+				},1100); 
 				setTimeout(function(){
 					$("#animate_focus").addClass("focus");
-				},3200); 
+				},2600); 
 				
 			});
 		}else{
@@ -26,6 +30,7 @@ $(function(){
 				shadow.removeClass("shadow_to_go");
 				blur.removeClass("blur_to_go");
 				content.removeClass("content_to_go");
+				flicker.removeClass("flicker");
 			});
 			
 		}
