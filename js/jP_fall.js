14b09c1051d1569e0fa7cb179f4f69ae6509d13c
eczn <1975845422@qq.com>
2016-08-27 00:34:42 +0800

    We Want You

--- js/jP_fall.js
+++ js/jP_fall.js
@@ -9,13 +9,16 @@
 function wallInit(domCanvasId){
 	var domCanvas = window.document.getElementById(domCanvasId);
 
-	domCanvas.width =  parseInt(window.innerWidth*2);
-	domCanvas.height = parseInt(window.innerHeight*2);
+	domCanvas.width =  parseInt($(window).innerWidth()*2);
+	domCanvas.height = parseInt($(window).innerHeight()*2);
 
 	var width = domCanvas.width*2,
 		height = domCanvas.height*2,
 		g = domCanvas.getContext('2d');
 
+
 	var draw = 0; 
 	this.draw = draw; 
+
+	// function 
 }
